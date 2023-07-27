8500cfc0825d78b5925d1e1db853831893801969
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2023-07-27 17:32:14 -0300

    Adição dos Models, Contact e Category

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+# Using decoretor admin.register
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name', 'phone'
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'first_name', 'last_name'
+    list_display_links = 'id', 'phone'
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
